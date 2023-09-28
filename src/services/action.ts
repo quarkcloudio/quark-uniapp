70655c342995954ca7f3fdf754c53168549b5b43
tangtanglove <dai_hang_love@126.com>
2023-09-28 15:24:04 +0800

    chore: wip

--- src/services/action.ts
+++ src/services/action.ts
@@ -1,27 +1,29 @@
-import { request } from '@/utils/request';
+import {
+  request
+}
+from '@/utils/request';
 
 // 发起GET请求
-export const get = async (params: any) => {
-	let url = params.url;
-	let data = params.data;
+export const get = async(params: any) =>{
+  let url = params.url;
+  let data = params.data;
+  var res = await request(url, {
+    method: 'GET',
+    data: data
+  });
 
-	var res = await request(url, {
-		method: 'GET',
-		data: data
-	});
-
-	return res;
+  return res;
 }
 
 // 发起POST请求
-export const post = async (params: any) => {
-	let url = params.url;
-	let data = params.data;
+export const post = async(params: any) =>{
+  let url = params.url;
+  let data = params.data;
 
-	var res = await request(url, {
-		method: 'POST',
-		data: data
-	});
+  var res = await request(url, {
+    method: 'POST',
+    data: data
+  });
 
-	return res;
+  return res;
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,24 +1,24 @@
 import config from "@/config/config"
 
 // 发起网络请求
-export const request = async (url: string, options: any) => {
-	if (url.indexOf("http") == -1) {
-		url = config.apiHost + url;
-	}
+export const request = async(url: string, options: any) =>{
+  if (url.indexOf("http") == -1) {
+    url = config.apiHost + url;
+  }
 
-	var token = uni.getStorageSync('token')
-	if (!token) {
-		token = ''
-	}
+  var token = uni.getStorageSync('token')
+  if(!token) {
+    token = ''
+  }
 
-	var res = await uni.request({
-		url: url,
-		data: options.data,
-		method: options.method,
-		header: {
-			Authorization: `Bearer ${token}`
-		}
-	});
+  var res = await uni.request({
+    url: url,
+    data: options.data,
+    method: options.method,
+    header: {
+      Authorization: `Bearer ${token}`
+    }
+  });
 
-	return res.data;
+  return res.data;
 }
