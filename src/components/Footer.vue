34e65cd255ccbbb7907e0515e7b8a3d4d283e861
tangtanglove <dai_hang_love@126.com>
2024-10-01 11:16:44 +0800

    chore: wip

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <nut-tabbar bottom safe-area-inset-bottom placeholder unactive-color="#7d7e80" active-color="#1989fa">
-    <nut-tabbar-item tab-title="标签" icon="home" :value="11" />
-    <nut-tabbar-item tab-title="标签" icon="home" :dot="true" />
-    <nut-tabbar-item tab-title="标签" icon="home" :value="110" />
+  <nut-tabbar bottom safe-area-inset-bottom placeholder>
+    <nut-tabbar-item tab-title="首页" icon="home" />
+    <nut-tabbar-item tab-title="分类" icon="category" />
+    <nut-tabbar-item tab-title="我的" icon="my" />
   </nut-tabbar>
 </template>
