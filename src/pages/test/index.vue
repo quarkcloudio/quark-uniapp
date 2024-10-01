08a5c2747d5c35b7941d18ea3bd66ff009dc69b7
tangtanglove <dai_hang_love@126.com>
2024-10-01 15:50:45 +0800

    chore: wip

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,10 +1,10 @@
+<script setup lang="ts">
+import { reactive, toRefs } from 'vue'
+</script>
+
 <template>
   <user-center />
 </template>
-
-<script setup lang="ts">
-import { reactive, toRefs } from 'vue';
-</script>
 
 <style lang="scss">
   @import "./index.scss";
