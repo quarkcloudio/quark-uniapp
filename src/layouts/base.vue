56d00e819bbe52e5cb6e755c6d758542b3e64882
tangtanglove <dai_hang_love@126.com>
2024-11-05 18:01:52 +0800

    chore: wip

--- src/layouts/base.vue
+++ src/layouts/base.vue
@@ -4,12 +4,6 @@
       dark: isDark,
     }"
   >
-    <main
-      p="x-4 y-10"
-      font-sans text="center"
-      color="gray-700 dark:gray-200"
-    >
-      <slot />
-    </main>
+    <slot />
   </div>
 </template>
