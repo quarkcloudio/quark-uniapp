0c0d4c0f68a3b9b6fb263ba9bc6e52162f8a00e5
tangtanglove <dai_hang_love@126.com>
2025-01-21 13:59:30 +0800

    chore: wip

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,12 +22,12 @@
         </nut-swiper-item>
       </nut-swiper>
     </view>
-    <nut-noticebar
+    <!-- <nut-noticebar
       pl-.5rem pr-.5rem
       text="NutUI 是京东风格的移动端组件库，使用 Vue 语言来编写可以在 H5，小程序平台上的应用，帮助研发人员提升开发效率，改善开发体验。"
       background="rgba(251, 248, 220, 1)"
       custom-color="#D9500B"
-    />
+    /> -->
     <view pb-.3rem pl-.5rem pr-.5rem pt-.3rem>
       <nut-swiper h-6rem overflow-hidden rounded-2xl :init-page="0" :loop="false" :pagination-visible="true" pagination-color="#fa2c19" pagination-unselected-color="#0000000f" auto-play="0">
         <nut-swiper-item class="nav-swiper-item">
