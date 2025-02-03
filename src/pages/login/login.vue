f0e878286f5fffa3e5d781ac611ce3e7a2342877
tangtanglove <dai_hang_love@126.com>
2025-02-03 14:10:22 +0800

    chore: wip

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,32 +1,62 @@
 <script setup lang="ts">
-import { post } from '~/services/action'
+import { get, post } from '~/services/action'
 
-const login = reactive({
+const toast = useToast()
+const captchaUrl = ref('')
+const formData = reactive({
   username: '',
   password: '',
+  captcha: {
+    id: '',
+    value: '',
+  },
 })
-const toast = useToast()
+
+onLoad((_) => {
+  refrashCaptcha()
+})
 
 async function accountLogin() {
   const result: any = await post({
     url: '/api/miniapp/login/login',
-    data: login,
+    data: formData,
+  })
+  if (result?.code !== 200) {
+    toast.error(result.msg)
+    refrashCaptcha()
+    return
+  }
+  console.log(result)
+}
+
+async function refrashCaptcha() {
+  const result: any = await get({
+    url: '/api/captcha/index/getId',
   })
   if (result?.code !== 200) {
     toast.error(result.msg)
     return
   }
-  console.log(result)
+  formData.captcha.id = result.data.captchaId
+  formData.captcha.value = ''
+  captchaUrl.value = `/api/captcha/index/getImage/${formData.captcha.id}?timestamp=${new Date().getTime()}`
 }
 </script>
 
 <template>
   <nut-form>
     <nut-form-item>
-      <nut-input v-model="login.username" class="nut-input-text" placeholder="用户名/邮箱/手机号" type="text" />
+      <nut-input v-model="formData.username" class="nut-input-text" placeholder="用户名/邮箱/手机号" type="text" />
     </nut-form-item>
     <nut-form-item>
-      <nut-input v-model="login.password" class="nut-input-text" placeholder="请输入密码" type="password" />
+      <nut-input v-model="formData.password" class="nut-input-text" placeholder="请输入密码" type="password" />
+    </nut-form-item>
+    <nut-form-item>
+      <nut-input v-model="formData.captcha.value" class="nut-input-text" placeholder="请输入验证码" type="text">
+        <template #right>
+          <img v-if="captchaUrl" :src="captchaUrl" @click="refrashCaptcha()">
+        </template>
+      </nut-input>
     </nut-form-item>
     <nut-cell>
       <nut-button block type="primary" @click="accountLogin()">
